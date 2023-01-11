98c9ba086eff790da50bd7ce0d1b8e61f6e0123a
jmanager25 <jmanager25@hotmail.com>
2023-01-11 17:36:16 +0000

    Remove the validate input function

--- run.py
+++ run.py
@@ -65,6 +65,7 @@ def validate_guess(player_guess, random_word):
     lenght of the player guess in more than random word and if the 
     word is already guessed.
     """
+    guessed_letters = []
     try:
         if not player_guess.isalpha():
             raise ValueError(
@@ -73,8 +74,8 @@ def validate_guess(player_guess, random_word):
         elif len(player_guess) > 1:
             if len(player_guess) != len(random_word):
                 raise ValueError(
-                    f"""Please enter a valid letter or word of the correct lenght. 
-                the word contains {len(random_word)} letters."""
+                    f"""Please enter a valid letter or word of the correct 
+                    lenght. The word contains {len(random_word)} letters."""
                 )
             else:
                 if player_guess in guessed_letters:
@@ -158,6 +159,7 @@ def main():
     start_game()
     get_words()
     get_player_input()
+    validate_guess(get_player_input(), get_words())
 
 
 main()
